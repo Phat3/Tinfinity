d5a6d3491da301936b4b9110b1fe2c8ab01f08e9
Sebastiano Mariani <seba91@iMac-di-Sebastiano-Mariani.local>
2015-04-04 13:41:44 +0200

    Initial Commit

---
+++ Tinfinity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinfinity
+//
+//  Created by Sebastiano Mariani on 04/04/15.
+//  Copyright (c) 2015 Sebastiano Mariani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

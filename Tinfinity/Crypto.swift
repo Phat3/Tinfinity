97db6a7845950a7fde8b16480af7981f5374032b
Sebastiano Mariani <seba0691@gmail.com>
2015-04-04 18:17:19 +0200

    fatta piccola prova pèer vedere se salva nel keychain e funziona
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

--- Tinfinity/Crypto.swift
+++ Tinfinity/Crypto.swift
@@ -5,8 +5,11 @@
 //  Created by Sebastiano Mariani on 04/04/15.
 //  Copyright (c) 2015 Sebastiano Mariani. All rights reserved.
 //
+//  Classe per gestire tutte le funzioni crittografiche dell applicazione
 
 import Foundation
+
+
 
 class Crypto{
     
@@ -25,18 +28,22 @@ class Crypto{
     
     //opzioni per la chiave pubblica
     //la vogliamo permanente e con la label specificata
-    let publicKeyParameters: [String: AnyObject] = [kSecAttrIsPermanent: true, kSecAttrApplicationTag: KeychainLabel.publicKey]
+    private let publicKeyParameters: [String: AnyObject] = [kSecAttrIsPermanent: true, kSecAttrApplicationTag: KeychainLabel.publicKey]
     
     //opzioni per la chiave privata
     //la vogliamo permanente e con la label specificata
-    let privateKeyParameters: [String: AnyObject] = [ kSecAttrIsPermanent: true, kSecAttrApplicationTag: KeychainLabel.privateKey]
+    private let privateKeyParameters: [String: AnyObject] = [ kSecAttrIsPermanent: true, kSecAttrApplicationTag: KeychainLabel.privateKey]
     
     //referenza alla chiave pubblica vera e propria
-    var publicKey : SecKeyRef?
+    private var publicKey : SecKeyRef?
     
     //referenza alla chiave private vera e propria
-    var privateKey : SecKeyRef?
+    private var privateKey : SecKeyRef?
     
+    private var blockSize : UInt?
+    
+    //costruttore che controlla se esistono gia le chiavi nel keychain
+    //se non esistyono le crea
     init(){
         
         //controlliamo se abbiamo la chiave gia salvata nel keychain
@@ -45,25 +52,12 @@ class Crypto{
         
         //se non esistono (prima volta che si attiva l applicazione) la generiamo
         if ( (self.privateKey == nil) || (self.publicKey == nil)){
-            
-            //definiamo i parametri generali
-            let parameters: [String: AnyObject] = [
-                kSecAttrKeyType: kSecAttrKeyTypeRSA,
-                kSecAttrKeySizeInBits: 2048,
-                kSecPublicKeyAttrs.takeUnretainedValue() as String: publicKeyParameters,
-                kSecPrivateKeyAttrs.takeUnretainedValue() as String: privateKeyParameters
-            ]
-            
-            var publicKeyPtr, privateKeyPtr: Unmanaged<SecKey>?
-            
-            //generiamo
-            SecKeyGeneratePair(parameters, &publicKeyPtr, &privateKeyPtr)
-            
-            //settiamo le referenze
-            self.privateKey = privateKeyPtr!.takeRetainedValue()
-            self.publicKey = publicKeyPtr!.takeRetainedValue()
+            println("entrato")
+            self.generateRSAKeys()
             
         }
+        
+        self.blockSize = SecKeyGetBlockSize(publicKey)
 
     }
     
@@ -94,24 +88,54 @@ class Crypto{
         }
     }
     
-    func generateRSAKeys(){
+    //genera la coppia di chiavi pubbliche e private RSA
+    private func generateRSAKeys(){
         
-        var publicKeyPtr:  Unmanaged<SecKey>?
-        var privateKeyPtr: Unmanaged<SecKey>?
-        
-        let parameters = [
-            String(kSecAttrKeyType): kSecAttrKeyTypeRSA,
-            String(kSecAttrKeySizeInBits): 2048
+        //definiamo i parametri generali
+        let parameters: [String: AnyObject] = [
+            kSecAttrKeyType: kSecAttrKeyTypeRSA,
+            kSecAttrKeySizeInBits: 2048,
+            kSecPublicKeyAttrs.takeUnretainedValue() as String: publicKeyParameters,
+            kSecPrivateKeyAttrs.takeUnretainedValue() as String: privateKeyParameters
         ]
         
+        var publicKeyPtr, privateKeyPtr: Unmanaged<SecKey>?
+        
+        //generiamo
+        SecKeyGeneratePair(parameters, &publicKeyPtr, &privateKeyPtr)
+        
+        //settiamo le referenze
+        self.privateKey = privateKeyPtr!.takeRetainedValue()
+        self.publicKey = publicKeyPtr!.takeRetainedValue()
+        
     }
     
-    func RSAEncrypt(message : String) -> String{
-        return "ci"
+    //cripta il messaggio usando RSA
+    func RSAEncrypt(message : String) -> [UInt8]{
+        //step1 : convertire il plaintext in utf8
+        let plainTextData = [UInt8](message.utf8)
+        //step2 : ricavare la lunghezza del testo in utf8
+        let plainTextDataLength = UInt(plainTextData.count)
+        //step3 : creare il buffer per il testo criptato
+        var encryptedData = [UInt8](count: Int(self.blockSize!), repeatedValue: 0)
+        //step4 : criptare
+        SecKeyEncrypt(publicKey, SecPadding(kSecPaddingPKCS1),  plainTextData, plainTextDataLength, &encryptedData, &self.blockSize!)
+        //ritorniamo il cipher
+        return encryptedData
+        
     }
     
-    func RSADecrypt(message : String) -> String{
-     return "ci"
+    //decripta il messaggio usando RSA
+    func RSADecrypt(encryptedData : [UInt8]) -> String{
+        //step1 : creare il buffer per i dati decriptati
+        var decryptedData = [UInt8](count: Int(self.blockSize!), repeatedValue: 0)
+
+        //decriptare
+        SecKeyDecrypt(privateKey, SecPadding(kSecPaddingPKCS1),encryptedData, self.blockSize!, &decryptedData, &self.blockSize!)
+        
+        let decryptedText = String(bytes: decryptedData, encoding:NSUTF8StringEncoding)
+        //ritorniamo il plain
+        return decryptedText!
     }
     
 
--- Tinfinity/ViewController.swift
+++ Tinfinity/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //DEBUG
+    
+        var testo = "ciao come va?"
+        
+        println("Testo in chiaro : \(testo)")
+        
+        var cryptoAPI = Crypto()
+        
+        var cipher: [UInt8] = cryptoAPI.RSAEncrypt(testo)
+        
+        var plain: String = cryptoAPI.RSADecrypt(cipher)
+        
+        println("Testo decriptato : \(plain)")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {

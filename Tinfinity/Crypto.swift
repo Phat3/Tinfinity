48b54b58e51efb8e5ac7e78603b95900dbcf7cb3
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2015-05-10 18:33:04 +0200

    Cleanup. Usiamo Alamofire per le richieste e SwiftyJSON per i JSON

--- Tinfinity/Crypto.swift
+++ Tinfinity/Crypto.swift
@@ -32,7 +32,7 @@ class Crypto{
     private let privateKeyParameters: [String: AnyObject] = [ kSecAttrIsPermanent as! String : true as Bool, kSecAttrApplicationTag as! String : KeychainLabel.privateKey]
     
     //reference to the public key
-     var publicKey : SecKeyRef?
+    var publicKey : SecKeyRef?
     
     //reference to the private key
     private var privateKey : SecKeyRef?
@@ -109,7 +109,7 @@ class Crypto{
     }
     
     /// Encrypt the essage using the RSA public key
-    func RSAEncrypt(message : String) -> [UInt8]{
+    func RSAEncrypt(message : String) -> String{
         //step1 : convertire il plaintext in utf8
         let plainTextData = [UInt8](message.utf8)
         //step2 : ricavare la lunghezza del testo in utf8
@@ -118,22 +118,62 @@ class Crypto{
         var encryptedData = [UInt8](count: self.blockSize, repeatedValue: 0)
         //step4 : criptare
         SecKeyEncrypt(self.publicKey as SecKey!, SecPadding(kSecPaddingPKCS1) as SecPadding,  plainTextData, Int(plainTextDataLength), &encryptedData, &self.blockSize)
+        var base64 = NSData(bytes: encryptedData, length: encryptedData.count).base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
         //ritorniamo il cipher
-        return encryptedData
+        return base64
         
     }
     
     /// Decrypt the message using RSA private key
-    func RSADecrypt(encryptedData : [UInt8]) -> String{
+    func RSADecrypt(encryptedDataBase64 : String) -> String{
+        
+        let base64dec = NSData(base64EncodedString: encryptedDataBase64, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)!
+        //prepariamo il buffer da riempire
+        var buffer = [UInt8]()
+        //referenza ai byte del cipher
+        let bytes = UnsafePointer<UInt8>(base64dec.bytes)
+        //riempiamo il buffer
+        for i in 0 ..< base64dec.length
+        {
+            buffer.append(bytes[i])
+        }
         //step1 : creare il buffer per i dati decriptati
         var decryptedData = [UInt8](count: self.blockSize, repeatedValue: 0)
         
         //decriptare
-        SecKeyDecrypt(privateKey, SecPadding(kSecPaddingPKCS1),encryptedData, self.blockSize, &decryptedData, &self.blockSize)
+        SecKeyDecrypt(privateKey, SecPadding(kSecPaddingPKCS1), buffer, self.blockSize, &decryptedData, &self.blockSize)
         
         let decryptedText = String(bytes: decryptedData, encoding:NSUTF8StringEncoding)
         //ritorniamo il plain
         return decryptedText!
+    }
+    
+    func send(){
+        
+        var http = HttpReqWraper()
+        
+        
+        //query paramenters
+        let query: [String: AnyObject] = [
+            kSecClass as! String : kSecClassKey as! String,
+            kSecAttrKeyType as! String : kSecAttrKeyTypeRSA,
+            kSecAttrApplicationTag as! String: "com.tinfinity.crypto.publickey",
+            kSecReturnRef as! String : true as Bool
+        ]
+        //get the reference to the publickey
+        var keyPtr: Unmanaged<AnyObject>?
+        //get bytes
+        let result = SecItemCopyMatching(query, &keyPtr)
+        //create a data object
+        var publickeyData = NSData(bytes: &keyPtr!, length: self.blockSize)
+        //encode it with base64
+        var base64PublicKey = publickeyData.base64EncodedStringWithOptions(nil)
+        
+        println(base64PublicKey)
+                    
+        //http.post("http://localhost:3000/auth/register-step1", params: ["key" : self.publicKey!])
+        
+        
     }
     
     
--- Tinfinity/ServerAPIController.swift
+++ Tinfinity/ServerAPIController.swift
@@ -2,57 +2,46 @@
 //  ServerAPIController.swift
 //  Tinfinity
 //
-//  Created by Alberto Fumagalli on 27/04/15.
-//  Copyright (c) 2015 Sebastiano Mariani. All rights reserved.
+//  @author Alberto Fumagalli
+//  @author Riccardo Mastellone
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class ServerAPIController{
     
     var FBToken : String
     let authenticationPath: String
-    let serverURL: String
+    let baseUrl: String
     
     init(FBAccessToken: String){
         FBToken = FBAccessToken
+        baseUrl = NSBundle.mainBundle().objectForInfoDictionaryKey("Server URL") as! String
         authenticationPath = NSBundle.mainBundle().objectForInfoDictionaryKey("Authentication Path") as! String
-        serverURL = NSBundle.mainBundle().objectForInfoDictionaryKey("Server URL") as! String
     }
     
     //Funzione che chiama il server e ritorna un'istanza di UserProfile
     func retrieveProfileFromServer(completion: (result: UserProfile) -> Void){
         
         var profileInfo : UserProfile?
-        var url: NSURL = NSURL(string: serverURL + authenticationPath)!
-        var bodyData = "token=" + FBToken
-        var request:NSMutableURLRequest = NSMutableURLRequest(URL:url)
-        request.HTTPMethod = "POST"
-        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding)
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-            data, response, error in
-            
-            if(error != nil) {
-                // If there is an error in the web request, print it to the console
-                println(error.localizedDescription)
-            }
-            var err: NSError?
-            if let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as? NSDictionary {
-                if(err != nil) {
-                    // If there is an error parsing JSON, print it to the console
-                    println("JSON Error \(err!.localizedDescription)")
+        
+        Alamofire.request(.POST, baseUrl + authenticationPath, parameters: ["token" : FBToken], encoding : .JSON)
+            .responseJSON { (request, response, data, error) in
+                
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    println(error!.localizedDescription)
                 }
-                if let json = jsonResult as? Dictionary<String, AnyObject> {
-                    if let name = json["name"] as? String {
-                        if let surname = json["surname"] as? String {
-                            profileInfo = UserProfile(name: name, surname: surname)
-                            completion(result: profileInfo!)
-                        }
-                    }
-                }
-            }
-        }        
-        task.resume()
+                
+                var json = JSON(data!)
+                
+                let name = json["name"].string;
+                let surname = json["surname"].string;
+                profileInfo = UserProfile(name: name!, surname: surname!)
+                completion(result: profileInfo!)
+        }
     }
     
 }
